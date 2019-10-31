7c24f6ccf4301ae27c6bf18afa71b4137ac6c2fe
ebasile96 <edoardo.basile96@gmail.com>
2019-10-31 11:39:25 +0100

    target system added

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadious);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadious);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadious);
+
+        //per ogni target visibile disegna una linea rossa
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTarget.position);
+        }
+    }
+}
