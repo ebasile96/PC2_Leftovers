2f893d95b4f713f491bcbd6ebf7d001db5a31402
ebasile96 <edoardo.basile96@gmail.com>
2019-12-15 17:42:39 +0100

    creata flowSM e UI manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     //instances and setups
     public static GameManager instance = null;
-
+    public Context newContext;
+    public Animator animController;
+    //managers
     public InputManager Inputmgr;
+    public UIManager UImgr;
     void Awake()
     {
         
         Singleton();
+        SetUpSM();
         InitManagers();
+    }
+
+    private void OnEnable()
+    {
+        if(instance == this)
+        {
+            GoToMainMenu += instance.HandleOnMainMenu;
+            GoToPause += instance.HandleOnPause;
+            GoToGameplay += instance.HandleOnGameplay;
+        }
     }
 
     public void InitManagers()
     {
         GetInputMgr();
+        GetUImgr();
     }
 
 
@@ -41,9 +57,65 @@ public class GameManager : MonoBehaviour
         return Inputmgr;
     }
 
+    public UIManager GetUImgr()
+    {
+        if (!UImgr)
+        {
+            return UImgr;
+        }
+
+        return UImgr;
+    }
     #endregion
 
+    #region SM
 
+    public class Context { }
 
+    void SetUpSM()
+    {
+        //Setup della state machine e del context
+        animController = GetComponent<Animator>();
+        
+        Context context = new Context()
+        {
+            
+        };
+        foreach (StateBehaviourBase state in animController.GetBehaviours<StateBehaviourBase>())
+        {
+            state.Setup(context);
+        }
+    }
 
+    //Delegates
+    public static Action GoToMainMenu;
+    public static Action GoToPause;
+    public static Action GoToGameplay;
+
+    //events
+    private void HandleOnMainMenu()
+    {
+        animController.SetTrigger("GoToMainMenu");
+    }
+
+    private void HandleOnGameplay()
+    {
+        animController.SetTrigger("GoToGameplay");
+    }
+
+    private void HandleOnPause()
+    {
+        animController.SetTrigger("GoToPause");
+    }
+    #endregion
+
+    private void OnDisable()
+    {
+        if (instance == this)
+        {
+            GoToMainMenu -= instance.HandleOnMainMenu;
+            GoToPause -= instance.HandleOnPause;
+            GoToGameplay -= instance.HandleOnGameplay;
+        }
+    }
 }
