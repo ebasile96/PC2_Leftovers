3b51f6919409a6dc8fcf78335d3a0cb2ea1d99dd
ebasile96 <edoardo.basile96@gmail.com>
2020-01-16 18:07:11 +0100

    creato poolManager

---
+++ Assets/Scripts/Managers/ObjectPooler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler SharedInstance;
+    public List<GameObject> PooledObjects;
+    public GameObject ObjectToPool;
+    public int AmountToPool;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    void Start()
+    {
+        PooledObjects = new List<GameObject>();
+        for (int i = 0; i < AmountToPool; i++)
+        {
+            GameObject obj = Instantiate(ObjectToPool);
+            obj.SetActive(false);
+            PooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < PooledObjects.Count; i++)
+        {
+            if (!PooledObjects[i].activeInHierarchy)
+            {
+                return PooledObjects[i];
+            }    
+        }
+        return null;
+    }
+ 
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    GameObject Enemy;
+
+    public void InstantiateEnemy()
+    {
+        Enemy = ObjectPooler.SharedInstance.GetPooledObject();
+        if(Enemy != null)
+        {
+            Enemy.transform.position = transform.position;
+            Enemy.transform.rotation = transform.rotation;
+            Enemy.SetActive(true);
+        }
+    }
+}
