38c82e800c4be9cc930395ecef481cacc05db278
GabrieleAncona <gabry.ancona@gmail.com>
2020-01-23 01:50:49 +0100

    creati animator controller companion e character , create Abstract class per animazioni

---
+++ Assets/Scripts/AbstractClass/IdleBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBase : MonoBehaviour
+{
+    public delegate void IdleEvent();
+    public IdleEvent OnIdle;
+}
---
+++ Assets/Scripts/AbstractClass/MovementBaseCompanion.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBaseCompanion : MonoBehaviour
+{
+    public delegate void MovementCompanionEvent();
+    public MovementCompanionEvent OnMovementCompanion;
+}
