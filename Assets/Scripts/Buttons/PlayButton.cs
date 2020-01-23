1a26b36cb858b7c297caf2ac7b65621b6210c2cd
Matteo Mosconi <matteo.mosconi.1999@gmail.com>
2020-01-23 16:36:10 +0100

    Montata Ui in scena UIGDTest

--- Assets/Scripts/Buttons/PlayButton.cs
+++ Assets/Scripts/Buttons/PlayButton.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//momentaneo
+using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
    public void PressPlay()
     {
         GameManager.GoToGameplay();
+    }
+
+    //momentaneo solo per far testare designer
+    public void PressUITest()
+    {
+        SceneManager.LoadScene("UIGDTest");
     }
 }
