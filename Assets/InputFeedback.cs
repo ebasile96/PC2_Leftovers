2b0186e512592afb0d861736ec49abcaefe53fc0
ebasile96 <edoardo.basile96@gmail.com>
2020-04-10 05:24:53 +0200

    implementato input feedback

---
+++ Assets/InputFeedback.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFeedback : MonoBehaviour
+{
+    public GameObject _graphic;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W))
+            _graphic.SetActive(true);
+        else
+            _graphic.SetActive(false);
+
+        transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 180 / Mathf.PI, 0);
+    }
+}
---
+++ Assets/InputFeedbackComp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFeedbackComp : MonoBehaviour
+{
+    public GameObject _graphic;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            _graphic.SetActive(true);
+        else
+            _graphic.SetActive(false);
+
+        transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("HorizontalPet"), Input.GetAxis("VerticalPet")) * 180 / Mathf.PI, 0);
+    }
+}
