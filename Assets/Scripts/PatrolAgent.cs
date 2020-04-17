000082731fb7eda3fda51e475f7ebb548d098b64
GabrieleAncona <gabry.ancona@gmail.com>
2020-04-17 18:59:52 +0200

    fixati navmesh obstacle , implementato pattern movimento enemy ranged(creata scena apposita) da fixare

---
+++ Assets/Scripts/PatrolAgent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+
+public class PatrolAgent : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    public bool isActive;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        if (isActive == true)
+        {
+            agent.destination = points[destPoint].position;
+            destPoint = (destPoint + 1) % points.Length;
+        }
+    }
+
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+}
