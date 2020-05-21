66798c786b12750b9d630a2e97353b01d81c8e4d
GabrieleAncona <gabry.ancona@gmail.com>
2020-05-21 23:22:52 +0200

    fixato bug input feedback con swap comandi

--- Assets/InputFeedback.cs
+++ Assets/InputFeedback.cs
@@ -15,10 +15,24 @@ public class InputFeedback : MonoBehaviour
     private void Update()
     {
         if (pc.movement * pc.moveSpeed != Vector3.zero)
-            _graphic.SetActive(true);
-        else
+        {
+            if (pc.isSwapPlayer == false)
+            {
+                transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 180 / Mathf.PI, 0);
+                _graphic.SetActive(true);
+                _graphicSwap.SetActive(false);
+            }
+            else if (pc.isSwapPlayer == true)
+            {
+                transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("HorizontalPet"), Input.GetAxis("VerticalPet")) * 180 / Mathf.PI, 0);
+                _graphic.SetActive(false);
+                _graphicSwap.SetActive(true);
+            }
+        }
+        else if (pc.movement * pc.moveSpeed == Vector3.zero)
+        {
             _graphic.SetActive(false);
-
-        transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 180 / Mathf.PI, 0);
+            _graphicSwap.SetActive(false);
+        }
     }
 }
--- Assets/InputFeedbackComp.cs
+++ Assets/InputFeedbackComp.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class InputFeedbackComp : MonoBehaviour
 {
     public GameObject _graphic;
+    public GameObject _graphicSwap;
     PetController petc;
+    public PlayerController player;
 
     private void Start()
     {
@@ -15,10 +17,26 @@ public class InputFeedbackComp : MonoBehaviour
     private void Update()
     {
         if (petc.movement * petc.moveSpeed != Vector3.zero)
-            _graphic.SetActive(true);
-        else
+        {
+            if (player.isSwapPlayer == false)
+            {
+                transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("HorizontalPet"), Input.GetAxis("VerticalPet")) * 180 / Mathf.PI, 0);
+                _graphic.SetActive(true);
+                _graphicSwap.SetActive(false);
+            }
+            else if (player.isSwapPlayer == true)
+            {
+                transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 180 / Mathf.PI, 0);
+                _graphic.SetActive(false);
+                _graphicSwap.SetActive(true);
+            }
+        }
+        else if (petc.movement * petc.moveSpeed == Vector3.zero)
+        {
             _graphic.SetActive(false);
-
-        transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("HorizontalPet"), Input.GetAxis("VerticalPet")) * 180 / Mathf.PI, 0);
+            _graphicSwap.SetActive(false);
+        }
     }
+
+
 }
